a7101f79fadab4022c67fee22f14d0d07aee90d9
kaito <sea_rr10_ch@outlook.com>
2023-05-10 03:56:31 +0900

    add constructor to Doller

--- murti-currency-app/app/Models/Money.php
+++ murti-currency-app/app/Models/Money.php
@@ -2,6 +2,15 @@
 
 namespace App\Models;
 
-class Doller {
+class Doller
+{
+    /**
+     * @var int
+     * 合計
+     */
+    protected $amount;
 
+    public function __consuruct(int $amount)
+    {
+    }
 }
