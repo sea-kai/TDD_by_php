bc285d2beecc9400196dbdf1936db534c80527b1
kaito <sea_rr10_ch@outlook.com>
2023-05-12 02:27:02 +0900

    Franc extend Money

--- murti-currency-app/app/Models/Money/Franc.php
+++ murti-currency-app/app/Models/Money/Franc.php
@@ -2,14 +2,8 @@
 
 namespace App\Models\Money;
 
-class Franc
+class Franc extends Money
 {
-    /**
-     * @var int
-     * 合計
-     */
-    private $amount;
-
     public function __construct(int $amount)
     {
         $this->amount = $amount;
@@ -22,15 +16,5 @@ class Franc
     public function times(int $multipiler)
     {
         return new Franc($this->amount * $multipiler);
-    }
-
-    /**
-     * @param Franc $object
-     * @return boolean
-     */
-    public function equals(Franc $object)
-    {
-        $franc = $object;
-        return $this->amount == $franc->amount;
     }
 }
