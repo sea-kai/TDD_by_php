80b9e8a785068dd41cbbd663f4b7ec47ff7c589e
kaito <sea_rr10_ch@outlook.com>
2023-05-22 03:10:56 +0900

    add equals() to Pair

--- murti-currency-app/app/Models/Pair.php
+++ murti-currency-app/app/Models/Pair.php
@@ -17,4 +17,30 @@ class Pair
         $this->from = $from;
         $this->to = $to;
     }
+
+    /**
+     * @param object $object
+     * @return boolean
+     */
+    public function equals(object $object)
+    {
+        $pair = $this->cast($object);
+        return $this->from == $pair->to && $this->to = $pair->from;
+    }
+
+    /**
+     * @return int
+     */
+    public function hashCode()
+    {
+        return 0;
+    }
+
+    public static function cast($obj): self
+    {
+        if (!($obj instanceof self)) {
+            throw new InvalidArgumentException("{$obj} is not instance of CastObject");
+        }
+        return $obj;
+    }
 }
