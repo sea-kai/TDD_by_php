5758777a27616db0a01ed8228947d5f9d5518650
kaito <sea_rr10_ch@outlook.com>
2023-06-06 02:26:28 +0900

    Sumクラスのplusメソッドの返り値を変更

--- multi-currency-app/app/Models/Money/Sum.php
+++ multi-currency-app/app/Models/Money/Sum.php
@@ -39,6 +39,6 @@ class Sum implements Expression
      */
     public function plus(Expression $addend): Expression
     {
-        return null;
+        return new Sum($this, $addend);
     }
 }
--- multi-currency-app/tests/Feature/MoneyTest.php
+++ multi-currency-app/tests/Feature/MoneyTest.php
@@ -142,4 +142,21 @@ class MoneyTest extends TestCase
 
         assertObjectEquals(Money::dollar(10), $result);
     }
+
+    /**
+     * @test
+     */
+    public function testSumPlusMoney()
+    {
+        $fiveBucks = Money::dollar(5);
+        $tenFrancs = Money::franc(10);
+
+        $bank = new Bank();
+        $bank->addRate('CHF', 'USD', 2);
+
+        $sum = (new Sum($fiveBucks, $tenFrancs))->plus($fiveBucks);
+        $result = $bank->reduce($sum, 'USD');
+
+        assertObjectEquals(Money::dollar(15), $result);
+    }
 }
